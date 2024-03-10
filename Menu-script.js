cae92a5ee7a96f8a2bb720ef21f4bba03387097c
Through2Dexter <162742084+Through2Dexter@users.noreply.github.com>
2024-03-10 22:24:57 +0000

    Java update

--- Menu-script.js
+++ Menu-script.js
@@ -4,5 +4,17 @@ let navlist = document.querySelector('navlist');
 menu.onclick = () => {
   menu.classList.toggle('bx-x');
   navlist.classList.toggle('open');
-}
- 
+};
+
+
+const sr = ScrollReveal ({
+  distance: '65px',
+  duration: 2600,
+  delay: 450,
+  reset: true
+});
+
+sr.reveal('.hero-text',{delay:200, original:'top'});
+sr.reveal('.boot',{delay:450, original:'top'});
+sr.reveal('.icons',{delay:500, original:'left'});
+sr.reveal('.scroll-down',{delay:500, original:'left'});
